795853b2fe68708ca5982ff32299a32632beda29
WendelMarx <99462534+WendelMarx@users.noreply.github.com>
2023-10-08 21:42:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    const html = document.documentElement
+
+   // if(hmtl.classList.contains('light')) {
+     //   html.classList.remove('light')
+    //} else {
+     //   html.classList.add('light')
+    
+
+    html.classList.toggle("light")
+}
